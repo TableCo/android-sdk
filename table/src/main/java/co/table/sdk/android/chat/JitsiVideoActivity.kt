1d8ab50252312e7584facb25473604f5b9c55079
Afify <a.afify1995@gmail.com>
2020-11-17 17:25:41 +0200

    Jitsi Implementation

--- table/src/main/java/co/table/sdk/android/chat/JitsiVideoActivity.kt
+++ table/src/main/java/co/table/sdk/android/chat/JitsiVideoActivity.kt
@@ -6,52 +6,52 @@ import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 import co.table.sdk.android.R
-import org.jitsi.meet.sdk.JitsiMeet
-import org.jitsi.meet.sdk.JitsiMeetActivity
-import org.jitsi.meet.sdk.JitsiMeetConferenceOptions
+import co.table.sdk.android.constants.Constants
+import org.jitsi.meet.sdk.*
 import java.net.MalformedURLException
 import java.net.URL
 
 class JitsiVideoActivity : AppCompatActivity() {
+    var firstLoad: Boolean = true
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jitsi_video)
 
         // Initialize default options for Jitsi Meet conferences.
         val serverURL: URL
-        val tenant: String = ""
-        val roomID: String = ""
-        val jwt: String = ""
+        val tenant: String = intent.getStringExtra(Constants.B_TENANT)!!
+        val roomID: String = intent.getStringExtra(Constants.B_ROOMID)!!
+        val jwt: String = intent.getStringExtra(Constants.B_JWT)!!
 
         serverURL = try {
-            // When using JaaS, replace "https://meet.jit.si" with the proper serverURL
-            URL("https://meet.jit.si")
+            URL("https://8x8.vc/")
         } catch (e: MalformedURLException) {
             e.printStackTrace()
             throw RuntimeException("Invalid server URL!")
         }
         val defaultOptions = JitsiMeetConferenceOptions.Builder()
                 .setServerURL(serverURL)
-            .setRoom("$tenant/$roomID")
-                .setToken(jwt)
                 .setWelcomePageEnabled(false)
-
                 .build()
         JitsiMeet.setDefaultConferenceOptions(defaultOptions)
-    }
+        val options = JitsiMeetConferenceOptions.Builder()
+            .setRoom("$tenant/$roomID")
+            .setToken(jwt)
+            .build()
+        this.firstLoad = false
+        JitsiMeetActivity.launch(this, options)
 
-    fun onButtonClick(v: View?) {
-        val editText = findViewById<EditText>(R.id.conferenceName)
-        val text = editText.text.toString()
 
-            // Build options object for joining the conference. The SDK will merge the default
-            // one we set earlier and this one when joining.
-            val options = JitsiMeetConferenceOptions.Builder()
-                    .setRoom("TiredBasketballsWeakenRelatively")
-                    .build()
-            // Launch the new activity with the given options. The launch() method takes care
-            // of creating the required Intent and passing the options.
-            JitsiMeetActivity.launch(this, options)
 
     }
+
+    override fun onResume() {
+        super.onResume()
+         if (!firstLoad){
+             finish()
+         }
+    }
+
+
 }
